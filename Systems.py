fbed7749bec90cb265b21bbb5f28c2e408638e95
stay-stay <157332995+stay-stay@users.noreply.github.com>
2024-09-28 22:59:16 -0400

    Add files via upload
    By following these steps, you have successfully integrated the SystemPerformanceManager into your ConstrainedDeviceApp. This allows your app to utilize the performance management functionality defined in the SystemPerformanceManager class.

---
+++ Systems.py
@@ -0,0 +1,23 @@
+import logging
+from programmingtheiot.cda.system.SystemPerformanceManager import SystemPerformanceManager
+
+
+class ConstrainedDeviceApp:
+    def __init__(self):
+        logging.info("Initializing CDA...")
+
+        self.sysPerfManager = SystemPerformanceManager()
+
+    def startApp(self):
+        logging.info("Starting CDA...")
+
+        self.sysPerfManager.startManager()
+
+        logging.info("CDA started.")
+
+    def stopApp(self, code: int):
+        logging.info("CDA stopping...")
+
+        self.sysPerfManager.stopManager()
+
+        logging.info("CDA stopped with exit code %s.", str(code))
